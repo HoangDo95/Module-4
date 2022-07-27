992b9a0beb9bdb90a4d4cf8893e3b876521bcc8a
dotronghoang95 <dotronghoang95@gmail.com>
2022-07-27 21:30:19 +0700

    ss10

--- ss10_session_cookie/exercise/cart/src/main/java/com/codegym/cart/controller/ProductController.java
+++ ss10_session_cookie/exercise/cart/src/main/java/com/codegym/cart/controller/ProductController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
 @SessionAttributes("cart")
@@ -27,17 +29,24 @@ public class ProductController {
     ProductService productService;
 
     @GetMapping("")
-    public String showList(Model model){
-        model.addAttribute("productList",productService.findAll());
+    public String showList(Model model, @CookieValue(value = "idProduct", defaultValue = "-1") int idProduct) {
+        if (idProduct != -1) {
+            model.addAttribute("historyProduct", productService.findById(idProduct).get());
+        }
+        model.addAttribute("productList", productService.findAll());
         return "/list";
     }
 
     @GetMapping("/detail/{id}")
-    public ModelAndView showDetail(@PathVariable int id){
-        ModelAndView modelAndView = new ModelAndView("view","product",productService.findById(id).get());
+    public ModelAndView showDetail(@PathVariable int id, HttpServletResponse response) {
+        Cookie cookie = new Cookie("idproduct",id + "");
+        cookie.setMaxAge(1*24*60*60);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        ModelAndView modelAndView = new ModelAndView("view", "product", productService.findById(id).get());
         return modelAndView;
-
     }
+
     @GetMapping("/add/{id}")
     public String addToCard(@PathVariable int id,
                             @ModelAttribute("cart") CartDto cart) {
